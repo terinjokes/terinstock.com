ed4046b36fc1379b523ab87ceb1ad7833eb9a1d0
Terin Stock <terinjokes@gmail.com>
2024-08-08 22:37:18 +0200

    remove nixpkgs related config
    I no longer use NixOS or nixpkgs, so no reason to keep this
    configuration in the repository.

--- nix/pkgs.nix
+++
@@ -1,6 +0,0 @@
-import (builtins.fetchTarball {
-  name = "nixos-unstable-2022-05-27";
-  url =
-    "https://github.com/NixOS/nixpkgs/archive/cce0667703fce3a1162dd252cf0864fdf83466ab.tar.gz";
-  sha256 = "1zrmdrnwr8q83bg7pgi9vnwdpzn1mdhqmcjm2g1w5idkyglksi6y";
-})
--- shell.nix
+++
@@ -1,5 +0,0 @@
-{ pkgs ? import ./nix/pkgs.nix { }, ... }:
-
-let d = import ./default.nix { inherit pkgs; };
-
-in pkgs.mkShell { buildInputs = [ d.hugo ]; }
